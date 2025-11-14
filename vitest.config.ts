ef8e422681bde413016ffbbf8c62cefaac7a5caa
Hugo C <235113615+hugo-stacks@users.noreply.github.com>
2025-11-14 17:40:10 +0100

    chore: sdk 3.10.0

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,4 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import {
   vitestSetupFilePath,
   getClarinetVitestsArgv,
@@ -23,11 +21,12 @@ import {
 
 export default defineConfig({
   test: {
-    environment: "clarinet", // use vitest-environment-clarinet
+    // use vitest-environment-clarinet
+    environment: "clarinet",
     pool: "forks",
-    poolOptions: {
-      forks: { singleFork: true },
-    },
+    // clarinet handles test isolation by resetting the simnet between tests
+    isolate: false,
+    maxWorkers: 1,
     setupFiles: [
       vitestSetupFilePath,
       // custom setup files can be added here
